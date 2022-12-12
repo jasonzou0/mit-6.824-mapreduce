dc809c48152dfdc61ae819e57b1455fdd61111db
Jingyu Zou <zjy@fb.com>
2022-12-11 23:25:40 -0500

    Actually run the mapper code and dump output to stdout

--- src/mr/worker.go
+++ src/mr/worker.go
@@ -5,6 +5,7 @@ import "log"
 import "net/rpc"
 import "hash/fnv"
 import "os"
+import "io/ioutil"
 import "strconv"
 
 //
@@ -30,24 +31,42 @@ func ihash(key string) int {
 //
 func Worker(mapf func(string, string) []KeyValue,
 	reducef func(string, []string) string) {
-	
-	hostname,_ := os.Hostname()
+
+	hostname, _ := os.Hostname()
 	worker_id := hostname + ":" + strconv.Itoa(os.Getpid())
 
 	// Your worker implementation here.
-	GetTask(worker_id)
+	worker_task, _ := GetTask(worker_id)
+	// TODO: properly handle error
+
+	if worker_task.Type == Mapper {
+		filename := worker_task.MapTask.InputFile
+		file, err := os.Open(filename)
+		if err != nil {
+			log.Fatalf("cannot open %v", filename)
+		}
+		content, err := ioutil.ReadAll(file)
+		if err != nil {
+			log.Fatalf("cannot read %v", filename)
+		}
+		file.Close()
+		kva := mapf(filename, string(content))
+		fmt.Println(kva)
+	}
 }
 
-func GetTask(worker_id string) {
+func GetTask(worker_id string) (WorkerTask, error) {
 	request := GetTaskRequest{worker_id}
 	reply := GetTaskResponse{}
 
-	ok := call("Coordinator.GetTask", &request, &reply)
+	ok, err := call("Coordinator.GetTask", &request, &reply)
 	if ok {
 		if reply.Task.Type == Mapper {
 			fmt.Printf("Get back Map task with file: %s\n", reply.Task.MapTask.InputFile)
 		}
+		return reply.Task, nil
 	}
+	return WorkerTask{}, err
 }
 
 //
@@ -55,7 +74,7 @@ func GetTask(worker_id string) {
 // usually returns true.
 // returns false if something goes wrong.
 //
-func call(rpcname string, args interface{}, reply interface{}) bool {
+func call(rpcname string, args interface{}, reply interface{}) (bool, error) {
 	// c, err := rpc.DialHTTP("tcp", "127.0.0.1"+":1234")
 	sockname := coordinatorSock()
 	c, err := rpc.DialHTTP("unix", sockname)
@@ -66,9 +85,9 @@ func call(rpcname string, args interface{}, reply interface{}) bool {
 
 	err = c.Call(rpcname, args, reply)
 	if err == nil {
-		return true
+		return true, nil
 	}
 
 	fmt.Println(err)
-	return false
+	return false, err
 }
