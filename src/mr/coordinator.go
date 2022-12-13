e3581e72a8f4ea25ff89bb2ead27e2efb7a3baae
Jingyu Zou <zjy@fb.com>
2022-12-13 01:05:26 -0500

    documentation change

--- src/mr/coordinator.go
+++ src/mr/coordinator.go
@@ -35,6 +35,10 @@ type Coordinator struct {
 	tasks_mu sync.Mutex
 	// They are laid out so that the first n_map tasks must be map tasas and remaining ones are reducer tasks.
 	// Reducer task with task_id i is guaranteed to be in all_tasks[n_map+i]
+	//
+	// Note: it is important that reducer task_id / shared id is 0-indexed because it has to match with the ihash()
+	// code in worker.go
+	//
 	all_tasks []InternalTask
 	n_map     int
 	n_reduce  int
@@ -271,25 +275,6 @@ func MakeCoordinator(files []string, nReduce int) *Coordinator {
 	}
 
 	// Your code here.
-
-	// Design:
-	// 1. iterate over the files
-	// 2. store status in a map.
-	// 2.5 figure out representation for map and reduce tasks
-	// 3. implement "get available task" rpc method - for both map and reduce tasks
-	//    each map & reduce task keeps track of the workers and their status.
-	// 4. implement "task done" rpc method - for communicating to coordinate that work is done
-
-	// Q: where is the worker pool maintained??? ans: in the coordinator
-	// Q: how should the communication model work between coordinator and worker?
-	// -  get_task: worker->coordinator
-	// -  task_done: worker->coordinator
-	// -  health_check: coordinator->worker but simplified to just waiting for 10s
-	// Q: how do i know if the intermediate file is done generated for a single reduce task?
-	// - all using files: mr-X-Y is the output of map task X for reduce task Y.
-	// - coordinator will periodically check if all map tasks for a given reduce shard is complete.
-	// Q: where does the sorting of immediate data happen?
-	// - should happen in the worker. ideally there is a shuffling worker whose job is sorting.
 
 	c.server()
 	return &c
