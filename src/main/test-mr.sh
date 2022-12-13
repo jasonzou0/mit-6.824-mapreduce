e44161d9c3f8a3675b508c420886bdb2a833f4c5
Jingyu Zou <zjy@fb.com>
2022-12-12 21:10:43 -0500

    wait for coordinator to finish in indexer test

--- src/main/test-mr.sh
+++ src/main/test-mr.sh
@@ -111,12 +111,14 @@ rm -f mr-out*
 
 echo '***' Starting indexer test.
 
-$TIMEOUT ../mrcoordinator ../pg*txt &
+$TIMEOUT ../mrcoordinator ../pg*txt & pid=$!
 sleep 1
 
 # start multiple workers
 $TIMEOUT ../mrworker ../../mrapps/indexer.so &
 $TIMEOUT ../mrworker ../../mrapps/indexer.so
+
+wait $pid
 
 sort mr-out* | grep . > mr-indexer-all
 if cmp mr-indexer-all mr-correct-indexer.txt
